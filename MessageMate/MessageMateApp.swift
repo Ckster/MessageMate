6adc5c6380997a0b6fbb7e8dec845a1d56e3bc99
Ckster <verleyee@msu.edu>
2023-01-21 14:22:59 -0700

    Initial Commit

---
+++ MessageMate/MessageMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessageMateApp.swift
+//  MessageMate
+//
+//  Created by Erick Verleye on 1/21/23.
+//
+
+import SwiftUI
+
+@main
+struct MessageMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
