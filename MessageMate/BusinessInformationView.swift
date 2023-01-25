e19d89c8fefc12993d86085a21b2045db49993c1
Ckster <verleyeerick@gmail.com>
2023-01-24 23:14:58 -0700

    Added formatted text fields for business information view

--- MessageMate/BusinessInformationView.swift
+++ MessageMate/BusinessInformationView.swift
@@ -21,23 +21,20 @@ struct BusinessInformationView: View {
 
     var body: some View {
         NavigationView {
-            GeometryReader {geometry in
+            GeometryReader { geometry in
                 VStack {
                     Text("Business Information").bold().font(.system(size: 33)).frame(width: geometry.size.width, height: geometry.size.height * 0.10, alignment: .leading).offset(x: 5).padding()
                     ScrollView {
                         ForEach(self.subViewDict.keys.sorted(), id:\.self) { category in
                             VStack {
-                                
                                 NavigationLink(destination: subViewDict[category]) {
                                     HStack {
                                         Text(category).foregroundColor(colorScheme == .dark ? Color.white : Color.black).font(.system(size: 23)).frame(width: geometry.size.width * 0.85, alignment: .leading)
                                         Image(systemName: "chevron.right").foregroundColor(.gray).imageScale(.small).offset(x: -5)
                                     }
                                 }
-                                
                                 HorizontalLine(color: .gray, height: 0.5)
-                                
-                            }
+                            }.offset(x: -20)
                         }
                     }
                 }
@@ -47,9 +44,133 @@ struct BusinessInformationView: View {
 }
 
 struct BusinessInfoSubView: View {
+    @Environment(\.colorScheme) var colorScheme
+    @State var businessName: String = ""
+    @State var location: String = ""
+    
     var body: some View {
-        Text("Business Info")
+        let textColor = colorScheme == .dark ? Color.white : Color.black
+        GeometryReader { geometry in
+            VStack {
+                Text("Business Info").bold().foregroundColor(textColor).font(.system(size: 25)).frame(width: geometry.size.width, alignment: .leading)
+                Text("Please input the following information about your business:").font(.system(size: 18)).frame(width: geometry.size.width, alignment: .leading).padding()
+                InputBoxView(heading: "Business Name", input: $businessName, width: geometry.size.width, height: geometry.size.height, autoCorrect: false).padding(.bottom)
+                InputBoxView(heading: "Location", input: $location, width: geometry.size.width, height: geometry.size.height)
+            }
+        }
     }
+}
+
+struct InputBoxView: View {
+    var heading: String = ""
+    @Binding var input: String
+    
+    let width: CGFloat
+    let height: CGFloat
+    var autoCorrect: Bool = true
+            
+    var body: some View {
+        VStack {
+            Text(heading).frame(width: width, alignment: .leading).font(.system(size: 15)).foregroundColor(.gray)
+            
+            NeumorphicStyleTextField(textField: TextField("", text: $input), sfName: nil, imageName: nil, textBinding: $input, placeholderText: heading, autoCorrect: autoCorrect).frame(width: width * 0.95).padding(.trailing)
+            
+//            TextField(heading, text: $input)
+//                .padding()
+//                .foregroundColor(.white)
+//                .background(Color.textBoxGgray)
+//                .cornerRadius(6)
+//                .overlay(
+//                    RoundedRectangle(cornerRadius: 6).stroke(self.isFieldFocused ? .white : .clear, lineWidth: 1)
+//                )
+//                .frame(width: width * 0.95).padding(.trailing)
+//                .focused($isFieldFocused)
+                
+        }.frame(width: width)
+    }
+}
+
+struct NeumorphicStyleTextField: View {
+    var textField: TextField<Text>
+    var sfName: String?
+    var imageName: String?
+    @Binding var textBinding: String
+    let placeholderText: String
+    var autoCorrect: Bool
+    @State var isTapped: Bool = false
+    @FocusState var isFieldFocused: Bool
+    
+    init(textField: TextField<Text>, sfName: String?, imageName: String?, textBinding: Binding<String>, placeholderText: String, autoCorrect: Bool = true) {
+        self.textField = textField
+        self.sfName = sfName
+        self.imageName = imageName
+        _textBinding = textBinding
+        self.placeholderText = placeholderText
+        self.autoCorrect = autoCorrect
+    }
+    
+    var body: some View {
+        HStack {
+            
+            if sfName != nil {
+                Image(systemName: sfName!)
+                    .foregroundColor(.darkShadow)
+            }
+            else {
+                if imageName != nil {
+                    Image(imageName!)
+                        .foregroundColor(.white)
+                }
+            }
+            textField.placeholder(when: textBinding.isEmpty) {Text(self.placeholderText).foregroundColor(.white)}.autocorrectionDisabled(!autoCorrect)
+            }
+            .padding()
+            .foregroundColor(.white)
+            .background(Color.textBoxGgray)
+            .cornerRadius(6)
+            .overlay(
+                RoundedRectangle(cornerRadius: 6).stroke(self.isFieldFocused ? .white : .clear, lineWidth: 1)
+            )
+            .padding(.trailing)
+            .focused($isFieldFocused)
+            
+    }
+}
+
+struct NeumorphicStyleButton: View {
+    var sfName: String?
+    var imageName: String?
+    let placeholderText: String
+    let geometry: GeometryProxy
+    
+    init(sfName: String?, imageName: String?, placeholderText: String, geometry: GeometryProxy) {
+        self.sfName = sfName
+        self.imageName = imageName
+        self.placeholderText = placeholderText
+        self.geometry = geometry
+    }
+    
+    var body: some View {
+        HStack {
+            if sfName != nil {
+                Image(systemName: sfName!)
+                    .foregroundColor(.darkShadow)
+            }
+            else {
+                if imageName != nil {
+                    Image(imageName!)
+                        .foregroundColor(.white)
+                }
+            }
+            Text(self.placeholderText).foregroundColor(.gray)
+            }
+            .padding()
+            .foregroundColor(.white)
+            .background(Color.black)
+            .cornerRadius(10)
+            .shadow(color: .gray, radius: 3, x: 2, y: 2)
+            .shadow(color: .gray, radius: 3, x: -2, y: -2)
+        }
 }
 
 struct PullToRefresh: View {
@@ -114,5 +235,41 @@ struct HorizontalLine: View {
 
     var body: some View {
         HorizontalLineShape().fill(self.color ?? .black).frame(minWidth: 0, maxWidth: .infinity, minHeight: height, maxHeight: height)
+    }
+}
+
+extension Color {
+    static let lightShadow = Color(red: 255 / 255, green: 255 / 255, blue: 255 / 255)
+    static let darkShadow = Color(red: 163 / 255, green: 177 / 255, blue: 198 / 255)
+    static let background = Color(red: 224 / 255, green: 229 / 255, blue: 236 / 255)
+    static let neumorphictextColor = Color(red: 132 / 255, green: 132 / 255, blue: 132 / 255)
+    static let textBoxGgray = Color(red: 31 / 255, green: 31 / 255, blue: 31 / 255)
+}
+
+extension View {
+    func placeholder<Content: View>(
+        when shouldShow: Bool,
+        alignment: Alignment = .leading,
+        @ViewBuilder placeholder: () -> Content) -> some View {
+
+        ZStack(alignment: alignment) {
+            placeholder().opacity(shouldShow ? 1 : 0)
+            self
+        }
+    }
+}
+
+extension View {
+    /// Applies the given transform if the given condition evaluates to `true`.
+    /// - Parameters:
+    ///   - condition: The condition to evaluate.
+    ///   - transform: The transform to apply to the source `View`.
+    /// - Returns: Either the original `View` or the modified `View` if the condition is `true`.
+    @ViewBuilder func `if`<Content: View>(_ condition: Bool, transform: (Self) -> Content) -> some View {
+        if condition {
+            transform(self)
+        } else {
+            self
+        }
     }
 }
