dd123359bd28fef0b14ca63be25f394be3622c61
Ckster <verleyeerick@gmail.com>
2023-02-18 18:45:02 -0700

    Making some progress on link adding

--- MessageMate/BusinessInformationView.swift
+++ MessageMate/BusinessInformationView.swift
@@ -6,6 +6,9 @@
 //
 
 import SwiftUI
+import FirebaseFirestore
+
+//"Website: awakenpermanentcosmetics.com. Scheduling appointment link: https://koalendar.com/e/schedule-my-pmu"
 
 struct BusinessInformationView: View {
     @EnvironmentObject var session: SessionStore
@@ -13,9 +16,9 @@ struct BusinessInformationView: View {
 
     let subViewDict: Dictionary<String, AnyView> = [
         "General": AnyView(BusinessInfoSubView()),
-//        "Business Info": BusinessInfoSubView(),
+        //"Business Info": BusinessInfoSubView(),
         "Links": AnyView(BusinessLinksSubView()),
-//        "Services": BusinessInfoSubView(),
+     //   "Services": BusinessInfoSubView(),
 //        "FAQs": BusinessInfoSubView(),
 //        "Do Not": BusinessInfoSubView(),
 //        "Frequently Changing": BusinessInfoSubView()
@@ -38,6 +41,12 @@ struct BusinessInformationView: View {
                                 HorizontalLine(color: .gray, height: 1.0)
                             }.padding(.leading).offset(x: -geometry.size.width * 0.03)
                         }
+//                        NavigationLink(destination: BusinessLinksSubView(width: geometry.size.width, height: geometry.size.height)) {
+//                            HStack {
+//                                Text("Links").foregroundColor(colorScheme == .dark ? Color.white : Color.black).font(.system(size: 23)).frame(width: geometry.size.width * 0.85, alignment: .leading)
+//                                Image(systemName: "chevron.right").foregroundColor(.gray).imageScale(.small).offset(x: -5)
+//                            }
+//                        }
                     }
                 }
             }
@@ -58,9 +67,9 @@ struct BusinessInfoSubView: View {
                 Text("Business Info").bold().foregroundColor(textColor).font(.system(size: 25)).frame(width: geometry.size.width, alignment: .leading)
                 Text("Please input the following information about your business:").font(.system(size: 18)).frame(width: geometry.size.width, alignment: .leading).padding()
                 
-                InputBoxView(heading: "Address", input: $address, width: geometry.size.width, height: geometry.size.height).padding(.bottom)
-                InputBoxView(heading: "Business Name", input: $businessName, width: geometry.size.width, height: geometry.size.height, autoCorrect: false).padding(.bottom)
-                InputBoxView(heading: "Industry", input: $industry, width: geometry.size.width, height: geometry.size.height, autoCorrect: false)
+                InputBoxView(heading: "Address", input: $address, width: geometry.size.width, height: geometry.size.height, fontSize: 15).padding(.bottom)
+                InputBoxView(heading: "Business Name", input: $businessName, width: geometry.size.width, height: geometry.size.height, fontSize: 15, autoCorrect: false).padding(.bottom)
+                InputBoxView(heading: "Industry", input: $industry, width: geometry.size.width, height: geometry.size.height, fontSize: 15, autoCorrect: false)
             }
         }
     }
@@ -68,41 +77,181 @@ struct BusinessInfoSubView: View {
 
 struct BusinessLinksSubView: View {
     @Environment(\.colorScheme) var colorScheme
-    @State var links: [DoubleInputBoxView] = [DoubleInputBoxView()]
+    @EnvironmentObject var session: SessionStore
+    @State var links: [DoubleInputBoxView] = []
     @State var businessName: String = ""
     @State var location: String = ""
+    @State var loading: Bool = true
+    @State var linkToDelete: UUID?
+    let db = Firestore.firestore()
     
     var body: some View {
         let textColor = colorScheme == .dark ? Color.white : Color.black
-        GeometryReader { geometry in
-            VStack {
-                Text("Links").bold().foregroundColor(textColor).font(.system(size: 25)).frame(width: geometry.size.width, alignment: .leading)
-                Text("Please add links to your businesse's web services:").font(.system(size: 18)).frame(width: geometry.size.width, alignment: .leading).padding()
-                ForEach(self.links, id:\.self.id) {
-                    link in
-                    link
+        GeometryReader {
+            geometry in
+            if self.loading {
+                LottieView(name: "97952-loading-animation-blue").onAppear {
+                    self.getLinks()
                 }
-                Text("+").onTapGesture {
-                    self.links.append(DoubleInputBoxView())
+            }
+            else {
+                VStack {
+                    Text("Links").bold().foregroundColor(textColor).font(.system(size: 25)).frame(width: geometry.size.width, alignment: .leading).padding(.leading)
+                    Text("Please add links to your businesse's web services:").font(.system(size: 18)).frame(width: geometry.size.width, alignment: .leading).padding(.leading).onChange(of: self.linkToDelete, perform: {newLinkId in
+                        let deletionIndex = self.links.firstIndex(where: { $0.id == newLinkId })
+                        if deletionIndex != nil {
+                            self.links.remove(at: deletionIndex!)
+                        }
+                    })
+                    
+                    ScrollView {
+                        ScrollViewReader {
+                            value in
+                            VStack(spacing: 70) {
+                                ForEach(self.links, id:\.self.id) {
+                                    link in
+                                    link.padding(.leading).id(link.id)
+                                }
+                                Image(systemName: "plus.circle").font(.system(size: 30)).onTapGesture {
+                                    let newDB = DoubleInputBoxView(keyType: "", value: "", deletable: true, linkToDelete: $linkToDelete)
+                                    self.links.append(newDB)
+                                    
+                                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                                        value.scrollTo(1)
+                                    }
+                                }.frame(maxWidth: .infinity).id(1)
+                            }
+                        }
+                    }
                 }
-            }.frame(height: geometry.size.height)
+//                .onDisappear(perform: {
+//                    self.updateLinks()
+//                })
+            }
         }
     }
+    
+    func getLinks() {
+        self.db.collection(Users.name).document("\(self.session.user.uid!)/\(Users.collections.BUSINESS_INFO.name)/\(Users.collections.BUSINESS_INFO.documents.FIELDS.name)").getDocument() {
+            doc, error in
+            print("Got Document")
+            if error == nil {
+                print("not nil")
+                let data = doc?.data()
+                if data != nil {
+                    print("Data not nil")
+                    let existingLinks = data![Users.collections.BUSINESS_INFO.documents.FIELDS.fields.LINKS] as? [String: String]
+                    
+                    if existingLinks != nil {
+                        var newExistingLinks: [DoubleInputBoxView] = []
+                        let urlTypes = Array(existingLinks!.keys)
+                        
+                        for linkType in urlTypes {
+                            newExistingLinks.append(DoubleInputBoxView(keyType: linkType, value: existingLinks![linkType]!, deletable: true, linkToDelete: $linkToDelete))
+
+                            if linkType == urlTypes.last {
+                                self.links = newExistingLinks
+                                self.loading = false
+                            }
+                        }
+                        
+                        if existingLinks?.count == 0 {
+                            self.links = [DoubleInputBoxView(keyType: "", value: "", deletable: false, linkToDelete: nil)]
+                            self.loading = false
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+//    func updateLinks() {
+//
+//        var newLinks: [String: String] = [:]
+//        for newLink in self.links {
+//            print(newLink.type)
+//            newLink.updateLink(uid: self.session.user.uid!)
+//        }
+//    }
 }
 
-struct DoubleInputBoxView: View {
+
+func keyToType(input: String) -> String {
+    let sep = input.split(separator: "_")
+    var newStr = ""
+    for w in sep {
+        newStr = newStr + w.capitalized
+        if w != sep.last {
+            newStr = newStr + " "
+        }
+    }
+    return newStr
+}
+
+
+func typeToKey(input: String) -> String {
+    let sep = input.split(separator: " ")
+    var newStr = ""
+    for w in sep {
+        newStr = newStr + w.lowercased()
+        if w != sep.last {
+            newStr = newStr + "_"
+        }
+    }
+    return newStr
+}
+
+
+struct DoubleInputBoxView: View, Equatable {
+    @EnvironmentObject var session: SessionStore
     let id = UUID()
-    @State var type: String = ""
-    @State var value: String = ""
+    var keyType: String
+    let deletable: Bool
+    @State var type: String
+    @State var value: String
+    @Binding var linkToDelete: UUID?
+    let db = Firestore.firestore()
+    
+    init (keyType: String, value: String, deletable: Bool, linkToDelete: Binding<UUID?>?) {
+        self.keyType = keyType
+        self.deletable = deletable
+        _type = State(initialValue: keyToType(input: keyType))
+        _value = State(initialValue: value)
+        self._linkToDelete = linkToDelete ?? Binding.constant(nil)
+    }
     
     var body: some View {
         GeometryReader {
             geometry in
             HStack {
-                InputBoxView(input: $type, width: geometry.size.width * 0.4, height: geometry.size.height)
-                InputBoxView(input: $value, width: geometry.size.width * 0.5, height: geometry.size.height)
+                InputBoxView(input: $type, width: geometry.size.width * 0.35, height: geometry.size.height, fontSize: 12)
+                InputBoxView(input: $value, width: geometry.size.width * 0.45, height: geometry.size.height, fontSize: 12).autocorrectionDisabled(true).autocapitalization(.none)
+                if self.deletable {
+                    Image(systemName: "minus.circle").font(.system(size: 25)).foregroundColor(.red).frame(width: geometry.size.width * 0.05).offset(y: 5).onTapGesture {
+                        self.linkToDelete = self.id
+                    }
+                }
             }
+            .onDisappear(perform: {
+                self.updateLink(uid: self.session.user.uid!)
+            })
         }
+    }
+    
+    func updateLink(uid: String) {
+        print(type)
+        print(typeToKey(input: self.type))
+        self.db.collection(Users.name).document("\(uid)/\(Users.collections.BUSINESS_INFO.name)/\(Users.collections.BUSINESS_INFO.documents.FIELDS.name)").updateData(
+            [Users.collections.BUSINESS_INFO.documents.FIELDS.fields.LINKS: [typeToKey(input: self.type): self.value]]
+        )
+    }
+    
+    static func ==(lhs: DoubleInputBoxView, rhs: DoubleInputBoxView) -> Bool {
+        return lhs.id == rhs.id
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(self.type)
     }
 }
 
@@ -113,12 +262,15 @@ struct InputBoxView: View {
     
     let width: CGFloat
     let height: CGFloat
+    let fontSize: CGFloat
     var autoCorrect: Bool = true
             
     var body: some View {
         VStack {
             Text(heading).frame(width: width, alignment: .leading).font(.system(size: 15)).foregroundColor(.gray)
-            NeumorphicStyleTextField(textField: TextField("", text: $input), sfName: nil, imageName: nil, textBinding: $input, placeholderText: heading, autoCorrect: autoCorrect).frame(width: width * 0.9).padding(.trailing)
+            NeumorphicStyleTextField(textField: TextField("", text: $input), sfName: nil, imageName: nil, textBinding: $input, placeholderText: heading, autoCorrect: autoCorrect, fontSize: fontSize)
+                //.frame(width: width * 0.9)
+                //.padding(.trailing)
         }.frame(width: width)
     }
 }
@@ -131,16 +283,18 @@ struct NeumorphicStyleTextField: View {
     @Binding var textBinding: String
     let placeholderText: String
     var autoCorrect: Bool
+    let fontSize: CGFloat
     @State var isTapped: Bool = false
     @FocusState var isFieldFocused: Bool
     
-    init(textField: TextField<Text>, sfName: String?, imageName: String?, textBinding: Binding<String>, placeholderText: String, autoCorrect: Bool = true) {
+    init(textField: TextField<Text>, sfName: String?, imageName: String?, textBinding: Binding<String>, placeholderText: String, autoCorrect: Bool = true, fontSize: CGFloat) {
         self.textField = textField
         self.sfName = sfName
         self.imageName = imageName
         _textBinding = textBinding
         self.placeholderText = placeholderText
         self.autoCorrect = autoCorrect
+        self.fontSize = fontSize
     }
     
     var body: some View {
@@ -168,6 +322,7 @@ struct NeumorphicStyleTextField: View {
                 RoundedRectangle(cornerRadius: 6).stroke(self.isFieldFocused ? self.colorScheme == .dark ? .white : .black : .clear, lineWidth: 1)
             )
             .padding(.trailing)
+            .font(.system(size: fontSize))
             .focused($isFieldFocused)
             
     }
--- MessageMate/FirebaseNamespaces.swift
+++ MessageMate/FirebaseNamespaces.swift
@@ -11,10 +11,12 @@ struct Users {
     // type attributes
     static let name = "users"
     static let fields = UsersFields.self
+    static let collections = UsersCollections.self
 
     // instance attributes
     var name: String
     var fields: UsersFields
+    var collections: UsersCollections
     var documentPath: String
 
     init(uid: String) {
@@ -22,6 +24,7 @@ struct Users {
         // This allows you to access the "type attributes" (static let ...) if you are using an instance of the class
         self.name = Users.name
         self.fields = UsersFields()
+        self.collections = UsersCollections()
         self.documentPath = "\(Users.name)/\(uid)"
     }
 }
@@ -33,6 +36,44 @@ struct UsersFields {
     static let ONBOARDING_COMPLETED = "onboarding_completed"
     static let LEGAL_AGREEMENT = "legal_agreement"
     static let FACEBOOK_USER_TOKEN = "facebook_user_token"
+}
+
+struct UsersCollections {
+    static let BUSINESS_INFO = BusinessInfoCollection.self
+}
+
+struct BusinessInfoCollection {
+    static let name = "business_information"
+    static let documents = BusinessInfoDocuments.self
+    
+    var name: String
+    var documents: BusinessInfoDocuments
+    
+    init() {
+        self.name = BusinessInfoCollection.name
+        self.documents = BusinessInfoDocuments()
+    }
+}
+
+struct BusinessInfoDocuments {
+    static let FIELDS = BusinessInfoFieldsDocument.self
+}
+
+struct BusinessInfoFieldsDocument {
+    static let name = "fields"
+    static let fields = BusinessInfoFields.self
+}
+
+struct BusinessInfoFields {
+    static let BUSINESS_ADDRESS = "business_address"
+    static let BUSINESS_NAME = "business_name"
+    static let FAQS = "faqs"
+    static let INDUSTRY = "industry"
+    static let LINKS = "links"
+    static let PRODUCTS_SERVICES = "products_services"
+    static let SENDER_CHARACTERISTICS = "sender_charachteristics"
+    static let SENDER_NAME = "sender_name"
+    static let SPECIFICS = "specifics"
 }
 
 
